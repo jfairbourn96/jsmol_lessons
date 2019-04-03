0479978d70a7ab7c665e71f56c556646d82dc613
Justin Fairbourn <jfairbourn96@bitbucket.org>
2019-04-02 21:38:09 -0600

    Added an acetylene directory and the formaldehyde.log. Need to redo acetylene.log

---
+++ acetylene/script.js
@@ -0,0 +1,42 @@
+// Authors: Justin Fairbourn and Scott Ensign
+// Created at Utah State University with support from the Chemistry department and the Center for Innovative Design and Instruction.
+let lesson = new Lesson();
+
+document.getElementById('restartButton').onclick = lesson.restart;
+document.getElementById('refreshButton').onclick = () => { lesson.execute(0); }
+document.getElementById('backButton').onclick = () => { lesson.execute(-1); }
+document.getElementById('nextButton').onclick = () => { lesson.execute(1); }
+document.getElementById('scriptButton').onclick = () => lesson.runCommand(document.getElementById('console').value);
+
+// TODO: Alter the instructions and add as many as necessary.
+lesson.addInstruction(
+  `This is a single water molecule.`,
+  `reset; spacefill 20%; wireframe 0.15; spin off; frank on; background=white; antialiasDisplay=true;` + lesson.shapeString
+);
+
+lesson.addInstruction(
+  `This is a spinning water molecule.`,
+  `spin on;`
+);
+
+lesson.addInstruction(
+  `Now it's spinning in multiple directions.`,
+  `set spinX 20; set spinY 20; set spinZ 0; spin on;`
+);
+
+lesson.addInstruction(
+  `Now it's spinning in multiple directions AND showing a surface`,
+  `set spinX 20; set spinY -10; set spinZ -10; spin on; isosurface vdw; isosurface translucent;`
+);
+
+lesson.addInstruction(
+  `See how executing the next instruction stops the spinning and removes the surface?`,
+  ``
+);
+
+lesson.addInstruction(
+  `Now you try, but with more interesting molecules!`,
+  `spin on;`
+);
+
+lesson.execute(0);
